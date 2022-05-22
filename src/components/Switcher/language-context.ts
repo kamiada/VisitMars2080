e30b37c02dce9cae09fe45e80295aa9cb5fc0ad5
kamiada <kamiada528@gmail.com>
2022-05-22 15:09:29 +0100

    json

--- src/components/Switcher/language-context.ts
+++ src/components/Switcher/language-context.ts
@@ -1,19 +1,22 @@
 import * as React from "react";
+import languages from '../../assets/text/text.json';
 
-export const languages = {
-    "ENG": [
-        { "exampleLine":"Hello World"}
-     ],
-     "FR": [
-         {
-             "exampleLine":"Bonjour le monde"
-         }
-     ],
-     "PL": [
-         {
-             "exampleLine":"Witaj Świecie"
-         }
-     ]
-  };
+// export const languages = {
+//     "ENG": [
+//         { "exampleLine":"Hello World"}
+//      ],
+//      "FR": [
+//          {
+//              "exampleLine":"Bonjour le monde"
+//          }
+//      ],
+//      "PL": [
+//          {
+//              "exampleLine":"Witaj Świecie"
+//          }
+//      ]
+//   };
   
-  export const LanguageContext = React.createContext(languages.ENG);
+//   export const LanguageContext = React.createContext(languages.ENG);
+
+export const LanguageContext = React.createContext(languages.ENG);
