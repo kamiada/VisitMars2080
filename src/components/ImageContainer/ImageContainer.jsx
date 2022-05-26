a8e4e6d48d7223200236b6c7e2d2e43e2b788ab2
kamiada <kamiada528@gmail.com>
2022-05-26 14:25:51 +0100

    I hate unsplash

--- src/components/ImageContainer/ImageContainer.jsx
+++ src/components/ImageContainer/ImageContainer.jsx
@@ -5,24 +5,23 @@ import { Container, CustomImage } from "./ImageContainer.styles";
 const api = createApi({
   accessKey: `${process.env.REACT_APP_ACCESS_KEY}`,
 });
-
 export const ImageContainer = () => {
-  const [data, setUnsplashResponse] = useState(null);
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetch(`https://source.unsplash.com/1600x900/?mars`);
+  const [data, setUnsplashResponse] = useState({url: 'empty'});
+
+useEffect(() => {
+    function fetchData() {
+      const data = fetch(`https://images.unsplash.com/photo-1615221025584-086389c99592?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bWFyc3x8fHx8fDE2NTM1NzA1ODI&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600`);
       return data;
     }
     fetchData()
       .then((result) => {
-        setUnsplashResponse(result);
+        setUnsplashResponse({url: result.url});
       })
       .catch(() => {
         console.log("something went wrong!");
       });
   }, []);
-
-  console.log(data);
+  console.log(data.url);
 
     if(data !== null || undefined ) {
         return (
