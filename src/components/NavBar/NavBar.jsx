d41fdbfbb71d1822be5e8342baa4eb9e341fc7ab
kamiada <kamiada528@gmail.com>
2022-05-31 23:07:23 +0100

    tiny progress

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Container } from './NavBar.styles';
 
 export const NavBar = () => {
-    
+    return (
+        <Container>
+            <ul>
+                <li>
+                    About.
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
+        </Container>
+    )
 }
